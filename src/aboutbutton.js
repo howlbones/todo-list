a702b61f2a63f7a5b57db53e704f257501e03a3c
Howl Kimura <howlkimuradev@gmail.com>
2023-08-07 12:03:33 +0300

    Minor changes to about hover section so it stays when hovering off a button

--- src/aboutbutton.js
+++ src/aboutbutton.js
@@ -1,17 +1,22 @@
 
 export function activateAboutButton() {
   const button = document.querySelector('button.about');
+  const container = document.querySelector('.about-popup-container');
   button.addEventListener('mouseenter', displayAbout);
-  button.addEventListener('mouseleave', hideAbout);
+  container.addEventListener('mouseleave', hideAbout);
 
 }
 
 function displayAbout() {
   const about = document.querySelector('.about-popup');
+  const button = document.querySelector('button.about');
+  button.classList.add('active');
   about.style.display = 'flex';
 }
 
 function hideAbout() {
   const about = document.querySelector('.about-popup');
+  const button = document.querySelector('button.about');
+  button.classList.remove('active');
   about.style.display = 'none';
 }
